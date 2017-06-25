91630d5675917071f509dedded0f0870d31e6310
TomaszMoroz <tmoroz688@gmail.com>
2017-06-25 21:39:30 +0200

    1 podejscie do licznika punktow gry

--- game.js
+++ game.js
@@ -1,6 +1,11 @@
 var gameBoard = document.querySelector('#game-board');
+var tablicaWynikow = document.querySelector('#tablica-wynikow')
 var size = 10;
 var tr, td;
+var wynik = 0;
+
+
+
 
 for (var i = 0; i < size; i += 1) {
     tr = document.createElement('tr');
@@ -16,8 +21,20 @@ for (var i = 0; i < 10; i += 1) {
     target.classList.add('coin')
 }
 
+
 gameBoard.addEventListener('click', function (event) {
     if (event.target.classList.contains('coin')) {
-        event.target.classList.remove('coin')
+            event.target.classList.remove('coin');
+            wynik += 1;
+             var score = [];
+            score.push(wynik);
+             var textWynik = document.createTextNode(score[score.length - 1]);
+             tablicaWynikow.appendChild(textWynik);
+                 if (tablicaWynikow.childElementCount > 2){
+                     tablicaWynikow.removeChild(tablicaWynikow.childNodes[0]);
+        }
+
+
     }
 })
+
