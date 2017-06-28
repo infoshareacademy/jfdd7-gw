90f0ac7e9e3e55863f4110218811866f456a5e76
piotrmichalek89 <piotrmichalek89@gmail.com>
2017-06-28 16:12:13 +0200

    timer setInterval() handleClick()

--- game.js
+++ game.js
@@ -3,7 +3,16 @@ var tablicaWynikow = document.querySelector('#tablica-wynikow')
 var size = 10;
 var tr, td;
 var wynik = 0;
-
+var timeCountdown = 3;
+var intervalId = setInterval(function() {
+    if (timeCountdown === 0 || wynik === 10) {
+        clearInterval(intervalId);
+        return
+    }
+    timeCountdown -= 1;
+    $('#timer').text(timeCountdown);
+    gameBoard.removeEventListener('click', handleClick);
+}, 1000);
 
 
 
@@ -22,18 +31,15 @@ for (var i = 0; i < 10; i += 1) {
     var target = emptyCells[Math.floor(Math.random() * emptyCells.length)];
     target.classList.add('coin')
 }
-
-
-gameBoard.addEventListener('click', function (event) {
+var handleClick = function (event) {
     if (event.target.classList.contains('coin')) {
-            event.target.classList.remove('coin');
-            wynik += 1;
+        event.target.classList.remove('coin');
+        wynik += 1;
         tablicaWynikow.innerText = wynik;
-
-
-
     }
-});
+}
+
+gameBoard.addEventListener('click', handleClick);
 
 //
 // $(document).ready(function() {
